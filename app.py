8f8f1687dbdd2c0799799afa9afa4a687b523e2e
Charles <cweng@stuy.edu>
2017-11-10 13:00:57 -0500

    fixed

--- app.py
+++ app.py
@@ -14,11 +14,11 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def load(arg):
-    uResp = urllib2.openurl('https://api.nasa.gov/planetary/apod?api_key=bBTrtmlfj4piBfET10iuLi3tsaHJMaKJiLGcguU3')
+def hello():
+    uResp = urllib2.urlopen('https://api.nasa.gov/planetary/apod?api_key=bBTrtmlfj4piBfET10iuLi3tsaHJMaKJiLGcguU3')
     d = json.loads(uResp.read())
     print d
-    return render_template("test.html", url=d[url], exp=d[explanation], date=d[date])
+    return render_template("test.html", url=d["url"], exp=d["explanation"], date=d["date"])
 
 
 if __name__ == "__main__":
