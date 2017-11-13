3a010a18efb9549586de04278d70e8723adf12e1
Charles <cweng@stuy.edu>
2017-11-13 03:09:12 -0500

    finished

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import json
-import urllib2
+from urllib2 import Request, urlopen
 
 '''
 Charles Weng
@@ -13,10 +13,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    uResp = urllib2.urlopen('https://api.nasa.gov/planetary/apod?api_key=bBTrtmlfj4piBfET10iuLi3tsaHJMaKJiLGcguU3')
-    d = json.loads(uResp.read())
-    print d
-    return render_template("test.html", url=d["url"], exp=d["explanation"], date=d["date"])
+    # headers for API
+    headers = {
+      'Accept': 'application/json',
+      'User-Agent': 'Mozilla/5.0'
+    }
+
+    page = 1
+    items = []
+    # loop through each page and get all the items into a list
+    while(page < 10):
+        # access the API
+        request = Request('https://private-anon-52cd4ff0cb-bindingofisaac.apiary-proxy.com/api/v1/item?page=' + str(page), headers=headers)
+        x = urlopen(request).read()
+        d = json.loads(x)
+        # add data and continue
+        items = items + d["data"]
+        page += 1
+    return render_template("test.html", items=items)
 
 
 if __name__ == "__main__":
